c93f8dd29437afd3e2232d633eb9911b225e2d0c
luckydut97 <luckydut97@gmail.com>
2024-05-14 17:47:25 +0900

    Feature: Add 특정 위경도 커스텀 마커 표시 메소드, Add 마커 클릭 팝업창 메소드

--- app/src/main/java/com/luckydut/hsbmap/CustomBottomSheetDialogFragment.kt
+++ app/src/main/java/com/luckydut/hsbmap/CustomBottomSheetDialogFragment.kt
@@ -1,4 +1,32 @@
 package com.luckydut.hsbmap
 
-class CustomBottomSheetDialogFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.luckydut.hsbmap.databinding.FragmentBottomSheetBinding
+
+class CustomBottomSheetDialogFragment : BottomSheetDialogFragment() {
+
+    private var _binding: FragmentBottomSheetBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentBottomSheetBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Set up your views here, e.g. binding.textViewTitle.text = "Title"
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
